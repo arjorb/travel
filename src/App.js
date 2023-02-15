01da283cef92a6a4c984eeec6175c805badce6c9
John U <arjorb@gmail.com>
2023-02-15 11:40:11 +0200

    feat:done working on the card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Navbar from './components/Navbar';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className='flex justify-center mt-10'>
       <div className='w-[550px] rounded-md overflow-hidden shadow-xl'>
         <Navbar />
+        <main className='w-[470px] mx-auto my-10'>
+          <Card />
+        </main>
       </div>
     </div>
   );
