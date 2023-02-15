38aa0c31ca4913bbc65380dcb5c39842a6e1aff3
John U <arjorb@gmail.com>
2023-02-15 10:53:47 +0200

    feat:done seting up the environment

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='App'>
+      <h1 className='text-3xl text-[#087b87] font-bold underline'>Seting up the Environment!</h1>
+    </div>
+  );
+}
+
+export default App;
